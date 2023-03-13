8a7b419b8ce51133050c1839aff76e30212eb5e4
grizli995 <grizli995@gmail.com>
2023-03-13 14:59:03 +0100

    Setup dependency injection

--- F5-StegSharp/UI.Console/Program.cs
+++ F5-StegSharp/UI.Console/Program.cs
@@ -1,7 +1,29 @@
-﻿public class Program
+﻿using Application.Common.Interfaces;
+using Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Create a new instance of the ServiceCollection class
+        var services = new ServiceCollection();
+
+        // Register the services provided by the class library projects
+        //services.AddSingleton<IService1, Service1>();
+        //services.AddSingleton<IService2, Service2>();
+        //services.AddSingleton<IService3, Service3>();
+
+        // Build the service provider
+        services.AddInfrastructureServices();
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Resolve a service and use it
+        //var service1 = serviceProvider.GetService<IService1>();
+        //service1.DoSomething();
+
+        Console.WriteLine("Application started.");
+        var service = serviceProvider.GetService<IColorTransformationService>();
+        service.RGBToYCbCr();
     }
 }
