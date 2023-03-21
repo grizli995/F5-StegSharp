e79b16d4d85a8a72894339b312fbeff81d790c70
grizli995 <grizli995@gmail.com>
2023-03-21 19:07:23 +0100

    Started implementation of F5 service. Work in progress.

--- F5-StegSharp/Application/Common/Interfaces/IF5.cs
+++ F5-StegSharp/Application/Common/Interfaces/IF5.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Application.Common.Interfaces
 {
     public interface IF5Service
     {
-        public void Embed();
+        public void Embed(Image image, string password, string text);
         public void Extract();
     }
 }
