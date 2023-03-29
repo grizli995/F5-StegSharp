f8554ff3ebdd131524505dfe7240007969cb514e
grizli995 <grizli995@gmail.com>
2023-03-29 13:22:45 +0200

    Added RunLengthEncodingService.cs for performing run-lenght encoding for the AC part of each MCU.

---
+++ F5-StegSharp/Application/Common/Interfaces/IRunLengthEncodingService.cs
@@ -0,0 +1,14 @@
+﻿using JpegLibrary;
+
+namespace Application.Common.Interfaces
+{
+    public interface IRunLengthEncodingService
+    {
+        /// <summary>
+        /// Performs run length encoding on a single MCU. 
+        /// </summary>
+        /// <param name="block"></param>
+        /// <returns>List of Tuple objects. Item1 is zeroCount, and Item2 is the value.</returns>
+        public List<Tuple<int, int>> Encode(JpegBlock8x8F block);
+    }
+}
---
+++ F5-StegSharp/Infrastructure/Services/RunLengthEncodingService.cs
@@ -0,0 +1,52 @@
+﻿using Application.Common.Interfaces;
+using Domain;
+using JpegLibrary;
+
+namespace Infrastructure.Services
+{
+    public class RunLengthEncodingService : IRunLengthEncodingService
+    {
+        public List<Tuple<int, int>> Encode(JpegBlock8x8F block)
+        {
+            var result = new List<Tuple<int, int>>();
+            var zeroCount = 0;
+
+            for (int i = 1; i < JpegSorting.JpegNaturalOrder.Length; i++)
+            {
+                var currentValue = (int)block[JpegSorting.JpegNaturalOrder[i]];
+
+                if (currentValue == 0)
+                {
+                    zeroCount++;
+                }
+                else
+                {
+                    result.Add(new Tuple<int, int>(zeroCount, currentValue));
+                    zeroCount = 0;
+                }
+
+                if (zeroCount > 15)
+                {
+                    result.Add(new Tuple<int, int>(zeroCount, currentValue));
+                    zeroCount = 0;
+                }
+            }
+
+            AddEndOfBlockMarker(result);
+
+            return result;
+        }
+
+        private static void AddEndOfBlockMarker(List<Tuple<int, int>> result)
+        {
+            var i = result.Count - 1;
+            while (result[i].Item2 == 0)
+            {
+                result.RemoveAt(i);
+                i--;
+            }
+
+            result.Add(new Tuple<int, int>(0, 0));
+        }
+    }
+}
