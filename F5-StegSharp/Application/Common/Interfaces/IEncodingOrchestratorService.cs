df2109e0a9dfdee7c4855a00af0c1f9d76d3890d
grizli995 <grizli995@gmail.com>
2023-04-19 16:49:36 +0200

    Added new decoding methods to EncodingOrchestratorService

--- F5-StegSharp/Application/Common/Interfaces/IEncodingOrchestratorService.cs
+++ F5-StegSharp/Application/Common/Interfaces/IEncodingOrchestratorService.cs
@@ -4,6 +4,18 @@ namespace Application.Common.Interfaces
 {
     public interface IEncodingOrchestratorService
     {
+        /// <summary>
+        /// Orchestrates the encoding process for all MCUs. Covers both run length encoding and huffman encoding.
+        /// </summary>
+        /// <param name="quantizedDCTData"></param>
+        /// <param name="bw"></param>
         public void EncodeData(DCTData quantizedDCTData, BinaryWriter bw);
+
+        /// <summary>
+        /// Orchestrates the decoding process for all MCUs. Covers both run length decoding and huffman decoding.
+        /// </summary>
+        /// <param name="quantizedDCTData"></param>
+        /// <param name="bw"></param>
+        public DCTData DecodeData(JpegInfo jpeg, BinaryReader bw);
     }
 }
--- F5-StegSharp/Infrastructure/Services/EncodingOrchestratorService.cs
+++ F5-StegSharp/Infrastructure/Services/EncodingOrchestratorService.cs
@@ -7,10 +7,16 @@ namespace Infrastructure.Services
     public class EncodingOrchestratorService : IEncodingOrchestratorService
     {
         private readonly IHuffmanEncodingService _huffmanEncodingService;
+        private readonly IHuffmanDecodingService _huffmanDecodingService;
+        private readonly IPaddingService _paddingService;
 
-        public EncodingOrchestratorService(IHuffmanEncodingService hhuffmanEncodingService)
+        public EncodingOrchestratorService(IHuffmanEncodingService hhuffmanEncodingService,
+            IHuffmanDecodingService huffmanDecodingService,
+            IPaddingService paddingService)
         {
             this._huffmanEncodingService = hhuffmanEncodingService;
+            this._paddingService = paddingService;
+            this._huffmanDecodingService = huffmanDecodingService;
         }
 
         public void EncodeData(DCTData quantizedDCTData, BinaryWriter bw)
@@ -38,9 +44,34 @@ namespace Infrastructure.Services
             _huffmanEncodingService.FlushBuffer(bw);
         }
 
+        public DCTData DecodeData(JpegInfo jpeg, BinaryReader br)
+        {
+            int mcuCount = CalculateMCUCount(jpeg);
+            var result = new DCTData(mcuCount);
+
+            var prevDc_Y = 0;
+            var prevDc_Cr = 0;
+            var prevDc_Cb = 0;
+
+            for (int i = 0; i < mcuCount; i++)
+            {
+                result.YDCTData[i] = DecodeMCUComponent(br, prevDc_Y, true);
+                result.CBDCTData[i] = DecodeMCUComponent(br, prevDc_Cb, false);
+                result.CRDCTData[i] = DecodeMCUComponent(br, prevDc_Cr, false);
+
+                prevDc_Y = (int)result.YDCTData[i][0];
+                prevDc_Cb = (int)result.CBDCTData[i][0];
+                prevDc_Cr = (int)result.CRDCTData[i][0];
+            }
+
+            return result;
+        }
+
+        #region
+
         private void EncodeMCUComponent(int prevDC, JpegBlock8x8F mcu, BinaryWriter bw, bool isLuminance)
         {
-            if(isLuminance)
+            if (isLuminance)
             {
                 _huffmanEncodingService.EncodeLuminanceDC((int)mcu[0], prevDC, bw);
                 _huffmanEncodingService.EncodeLuminanceAC(mcu, bw);
@@ -49,8 +80,36 @@ namespace Infrastructure.Services
             {
                 _huffmanEncodingService.EncodeChrominanceDC((int)mcu[0], prevDC, bw);
                 _huffmanEncodingService.EncodeChrominanceAC(mcu, bw);
-
             }
         }
+
+        private int CalculateMCUCount(JpegInfo jpeg)
+        {
+            int paddedHeight = _paddingService.CalculatePaddedDimension(jpeg.Height);
+            int paddedWidth = _paddingService.CalculatePaddedDimension(jpeg.Width);
+            var mcuCount = paddedHeight * paddedWidth / 64;
+            return mcuCount;
+        }
+
+        private JpegBlock8x8F DecodeMCUComponent(BinaryReader br, int prevDc, bool isLuminance)
+        {
+            int dc;
+            JpegBlock8x8F result;
+            if (isLuminance)
+            {
+                dc = _huffmanDecodingService.DecodeLuminanceDC(prevDc, br);
+                result = _huffmanDecodingService.DecodeLuminanceAC(br);
+            }
+            else
+            {
+                dc = _huffmanDecodingService.DecodeChrominanceDC(prevDc, br);
+                result = _huffmanDecodingService.DecodeChrominanceAC(br);
+            }
+
+            result[0] = dc;
+            return result;
+        }
+
+        #endregion
     }
 }
