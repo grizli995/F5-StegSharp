b8e14f19d8c844b6f74bd792f9e9a8038e68902a
grizli995 <grizli995@gmail.com>
2023-04-24 18:34:27 +0200

    Added new extension method for BinaryReader in BinaryReaderExtensions.cs

---
+++ F5-StegSharp/Infrastructure/Util/Extensions/BinaryReaderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Util.Extensions
+{
+    public static class BinaryReaderExtensions
+    {
+        public static int Read2Bytes(this BinaryReader reader)
+        {
+            var upper = reader.ReadByte();
+            var lower = reader.ReadByte();
+
+            var result = (upper << 8) | lower;
+
+            return result;
+        }
+    }
+}
