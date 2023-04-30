b78e87334f1fd171b3c70086ec1267bc14c951c9
grizli995 <grizli995@gmail.com>
2023-04-30 13:40:40 +0200

    Fixed values in EmbeddingRateTable

--- F5-StegSharp/Infrastructure/Util/EmbeddingRateTable.cs
+++ F5-StegSharp/Infrastructure/Util/EmbeddingRateTable.cs
@@ -7,15 +7,15 @@ namespace Infrastructure.Util
         public static readonly EmbeddingRateRecord[] Table =
         {
             // n, k, changeDensity, embeddingRate, embeddingEfficiency
-            new EmbeddingRateRecord(1,1,50, 100, 2),
-            new EmbeddingRateRecord(2, 3, 25.00, 66.67, 2.67),
-            new EmbeddingRateRecord(3, 7, 12.50, 42.86, 3.43),
-            new EmbeddingRateRecord(4, 15, 6.25, 26.67, 4.27),
-            new EmbeddingRateRecord(5, 31, 3.12, 16.13, 5.16),
-            new EmbeddingRateRecord(6, 63, 1.56, 9.52, 6.09),
-            new EmbeddingRateRecord(7, 127, 0.78, 5.51, 7.06),
-            new EmbeddingRateRecord(8, 255, 0.39, 3.14, 8.03),
-            new EmbeddingRateRecord(9, 511, 0.20, 1.76, 9.02)
+            new EmbeddingRateRecord(1, 1, 0.50, 1, 2),
+            new EmbeddingRateRecord(2, 3, 0.2500, 0.6667, 2.67),
+            new EmbeddingRateRecord(3, 7, 0.1250, 0.4286, 3.43),
+            new EmbeddingRateRecord(4, 15, 0.0625, 0.2667, 4.27),
+            new EmbeddingRateRecord(5, 31, 0.0312, 0.1613, 5.16),
+            new EmbeddingRateRecord(6, 63, 0.0156, 0.0952, 6.09),
+            new EmbeddingRateRecord(7, 127, 0.0078, 0.0551, 7.06),
+            new EmbeddingRateRecord(8, 255, 0.0039, 0.0314, 8.03),
+            new EmbeddingRateRecord(9, 511, 0.0020, 0.0176, 9.02)
         };
     }
 }
